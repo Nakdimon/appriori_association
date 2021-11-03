98f210860b2339de05863b6dec76242132ed8f23
Nakdimon <gjherrera.i@gmail.com>
2021-11-03 23:14:36 +0800

    Added stage_5, ordered code, calculated confidence

--- association.py
+++ association.py
@@ -16,19 +16,6 @@ items = sorted(
     [item for sublist in records for item in sublist if item != 'nan'])
 
 
-def stage_1(items, minimum_support_count):
-    c1 = {i: items.count(i) for i in items}
-    l1 = {}
-    for key, value in c1.items():
-        if value >= minimum_support_count:
-            l1[key] = value
-
-    return c1, l1
-
-
-c1, l1 = stage_1(items, minimum_support_count)
-
-
 def sublist(lst1, lst2):
     return set(lst1) <= set(lst2)
 
@@ -42,6 +29,16 @@ def check_subset_frequency(itemset, l, n):
         if not iter1 in l:
             return False
     return True
+
+
+def stage_1(items, minimum_support_count):
+    c1 = {i: items.count(i) for i in items}
+    l1 = {}
+    for key, value in c1.items():
+        if value >= minimum_support_count:
+            l1[key] = value
+
+    return c1, l1
 
 
 def stage_2(l1, records, minimum_support_count):
@@ -61,9 +58,6 @@ def stage_2(l1, records, minimum_support_count):
                 l2[key] = value
 
     return c2, l2
-
-
-c2, l2 = stage_2(l1, records, minimum_support_count)
 
 
 def stage_3(l2, records, minimum_support_count):
@@ -86,6 +80,74 @@ def stage_3(l2, records, minimum_support_count):
     return c3, l3
 
 
-c3, l3 = stage_3(l2, records, minimum_support_count)
+def stage_4(l3, records, minimum_support_count):
+    l3 = list(l3.keys())
+    L3 = sorted(list(set([item for t in l3 for item in t])))
+    L3 = list(itertools.combinations(L3, 4))
+    c4 = {}
+    l4 = {}
+    for iter1 in L3:
+        count = 0
+        for iter2 in records:
+            if sublist(iter1, iter2):
+                count += 1
+        c4[iter1] = count
+    for key, value in c4.items():
+        if value >= minimum_support_count:
+            if check_subset_frequency(key, l3, 3):
+                l4[key] = value
 
-print(c3, l3)
+    return c4, l4
+
+
+def stage_5(l4, records, minimum_support_count):
+    l4 = list(l4.keys())
+    L4 = sorted(list(set([item for t in l4 for item in t])))
+    L4 = list(itertools.combinations(L4, 5))
+    c5 = {}
+    l5 = {}
+    for iter1 in L4:
+        count = 0
+        for iter2 in records:
+            if sublist(iter1, iter2):
+                count += 1
+        c5[iter1] = count
+    for key, value in c5.items():
+        if value >= minimum_support_count:
+            if check_subset_frequency(key, l4, 4):
+                l5[key] = value
+
+    return c5, l5
+
+
+c1, l1 = stage_1(items, minimum_support_count)
+c2, l2 = stage_2(l1, records, minimum_support_count)
+c3, l3 = stage_3(l2, records, minimum_support_count)
+c4, l4 = stage_4(l3, records, minimum_support_count)
+c5, l5 = stage_5(l4, records, minimum_support_count)
+
+print("L1 => ", l1)
+print("L2 => ", l2)
+print("L3 => ", l3)
+print("L4 => ", l4)
+print("L5 => ", l5)
+
+itemlist = {**l1, **l2, **l3, **l4}
+sets = []
+for iter1 in list(l4.keys()):
+    subsets = list(itertools.combinations(iter1, 2))
+    sets.append(subsets)
+
+
+def support_count(itemset, itemlist):
+    return itemlist[itemset]
+
+
+list_l4 = list(l4.keys())
+for i in range(0, len(list_l4)):
+    for iter1 in sets[i]:
+        a = iter1
+        b = set(list_l4[i]) - set(iter1)
+        confidence = (support_count(
+            list_l4[i], itemlist)/support_count(iter1, itemlist))*100
+        print("Confidence{}->{} = ".format(a, b), confidence)
