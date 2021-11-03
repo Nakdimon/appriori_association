cf44d5ee18b90eeee8f824b1a5e7eff1e932ce54
Nakdimon <gjherrera.i@gmail.com>
2021-11-03 22:56:50 +0800

    Adding stage 3

--- association.py
+++ association.py
@@ -28,4 +28,64 @@ def stage_1(items, minimum_support_count):
 
 c1, l1 = stage_1(items, minimum_support_count)
 
-print(c1, l1)
+
+def sublist(lst1, lst2):
+    return set(lst1) <= set(lst2)
+
+
+def check_subset_frequency(itemset, l, n):
+    if n > 1:
+        subsets = list(itertools.combinations(itemset, n))
+    else:
+        subsets = itemset
+    for iter1 in subsets:
+        if not iter1 in l:
+            return False
+    return True
+
+
+def stage_2(l1, records, minimum_support_count):
+    l1 = sorted(list(l1.keys()))
+    L1 = list(itertools.combinations(l1, 2))
+    c2 = {}
+    l2 = {}
+    for iter1 in L1:
+        count = 0
+        for iter2 in records:
+            if sublist(iter1, iter2):
+                count += 1
+        c2[iter1] = count
+    for key, value in c2.items():
+        if value >= minimum_support_count:
+            if check_subset_frequency(key, l1, 1):
+                l2[key] = value
+
+    return c2, l2
+
+
+c2, l2 = stage_2(l1, records, minimum_support_count)
+
+
+def stage_3(l2, records, minimum_support_count):
+    l2 = list(l2.keys())
+    L2 = sorted(list(set([item for t in l2 for item in t])))
+    L2 = list(itertools.combinations(L2, 3))
+    c3 = {}
+    l3 = {}
+    for iter1 in L2:
+        count = 0
+        for iter2 in records:
+            if sublist(iter1, iter2):
+                count += 1
+        c3[iter1] = count
+    for key, value in c3.items():
+        if value >= minimum_support_count:
+            if check_subset_frequency(key, l2, 2):
+                l3[key] = value
+
+    return c3, l3
+
+
+c3, l3 = stage_3(l2, records, minimum_support_count)
+
+print(c3, l3)
