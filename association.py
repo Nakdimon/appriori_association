9cc07e151bb6bf6799ed11720751495760a27612
Nakdimon <gjherrera.i@gmail.com>
2021-11-03 22:52:16 +0800

    created stage_1

--- association.py
+++ association.py
@@ -14,3 +14,18 @@ for i in range(0, len(data)):
                    for j in range(INITIAL_RANGE, FINAL_RANGE)])
 items = sorted(
     [item for sublist in records for item in sublist if item != 'nan'])
+
+
+def stage_1(items, minimum_support_count):
+    c1 = {i: items.count(i) for i in items}
+    l1 = {}
+    for key, value in c1.items():
+        if value >= minimum_support_count:
+            l1[key] = value
+
+    return c1, l1
+
+
+c1, l1 = stage_1(items, minimum_support_count)
+
+print(c1, l1)
